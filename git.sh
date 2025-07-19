3d91fed1776014f228a5a587d8526dfc797e4949
bees-xiv <bees-xiv@users.noreply.github.com>
2025-07-19 22:40:26 +0200

    w

--- git.sh
+++ git.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+d="$(dirname "$(dirname "$(realpath "$0")")")"
+
 git config user.name "bees-xiv"
 git config user.email "bees-xiv@users.noreply.github.com"
 git config commit.gpgsign false
-git config core.sshcommand "ssh -i /stuff/secrets/git-ssh-key-bees-xiv"
+git config core.sshcommand "ssh -i $d/bees-xiv-git"
 git config tag.gpgsign false
 git config user.signingkey ""
 
-ssh-add /stuff/secrets/git-ssh-key-bees-xiv
+ssh-add "$d/bees-xiv-git"
